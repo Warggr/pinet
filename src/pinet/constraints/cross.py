15e3bdaf63fce54a818f32593e7e917af7ded1b2
Pierre Ballif <pierre@ballif.eu>
2026-03-20 10:39:15 +0100

    wip: cross constraints, approach 2

--- src/pinet/constraints/cross.py
+++ src/pinet/constraints/cross.py
@@ -1,5 +1,7 @@
 """Cross constraint module."""
 
+from dataclasses import dataclass
+
 import jax.numpy as jnp
 from jax import lax
 
@@ -8,7 +10,8 @@ from pinet.dataclasses import ProjectionInstance
 from .base import Constraint
 
 
-class CrossConstraint(Constraint):
+@dataclass
+class CrossConstraintParams:
     """Cross constraint set.
 
     The cross constraint set is defined as:
@@ -19,33 +22,50 @@ class CrossConstraint(Constraint):
     w >= 0
     """
 
+    M: jnp.ndarray
+    q: jnp.ndarray
+    idxs_z: slice
+
+    def __post_init__(self):
+        """Check dimensions of parameters."""
+        assert (
+            self.M.ndim == 3
+        ), "M is a matrix with shape (batch_size, n_constraints, dimension)."
+
+
+class CrossConstraint(Constraint):
+    """Lifted cross constraint set.
+
+    Defined by:
+    z := x[idxs_z]
+    w := x[idxs_w]
+    <z, w> = 0
+    z >= 0
+    w >= 0
+    It is assumed that w are additional variables from lifting,
+    and that the constraint w = M z - q is enforced somewhere else.
+    """
+
     def __init__(
         self,
-        M: jnp.ndarray,
-        q: jnp.ndarray,
         idxs_z: slice,
+        idxs_w: slice,
     ) -> None:
         """Initialize the equality constraint.
 
         Args:
-            M (jnp.ndarray): Left hand side matrix.
-                Shape (batch_size, n_constraints, dimension).
-            q (jnp.ndarray): Right hand side vector.
-                Shape (batch_size, n_constraints, 1).
             idxs_z: the indices of y to be considered, i.e. z = y[idxs_z]
+            idxs_w: idem.
         """
-        assert M is not None, "Matrix A must be provided."
-
-        self.M = M
-        self.q = q
         self.idxs_z = idxs_z
-        self.setup()
+        self.idxs_w = idxs_w
 
-    def setup(self) -> None:
-        """Sets up the equality constraint."""
-        assert (
-            self.M.ndim == 3
-        ), "M is a matrix with shape (batch_size, n_constraints, dimension)."
+        def simple_slice_len(sl: slice):
+            assert sl.step in (1, None)
+            assert sl.stop >= 0 and sl.start >= 0
+            return sl.stop - sl.start
+
+        assert simple_slice_len(self.idxs_w) == simple_slice_len(self.idxs_z)
 
     def project(self, yraw: ProjectionInstance) -> ProjectionInstance:
         """Project onto constraints.
@@ -57,56 +77,34 @@ class CrossConstraint(Constraint):
         Returns:
             ProjectionInstance: The projected point for each point in the batch.
         """
-
-        def project_unbatched(x):
-            # The unbatched version is necessary
-            # because lax.switch has no batched version.
-            from jax.numpy import dot
-
-            def case_1(w, z):
-                from jax.numpy.linalg import norm
-
-                lambd = (dot(w, w) + dot(z, z) - norm(w + z) * norm(w - z)) / (
-                    2 * dot(w, z)
-                )
-                factor = 1 / (1 - lambd**2)
-                return factor * (w - lambd * z), factor * (z - lambd * w)
-
-            def case_2(w, z):
-                return jnp.zeros_like(w), z
-
-            z = x[self.idxs_z]
-            w = self.M @ z - self.q
-            is_zero = dot(w, z) == 0
-            norm_is_equal = jnp.all(w == z) | jnp.all(w == -z)
-            cond = jnp.where(is_zero, 0, jnp.where(norm_is_equal, 2, 1))
-            w, z = lax.switch(
-                cond,
-                [
-                    lambda w, z: (w, z),
-                    case_1,
-                    case_2,
-                ],
-                w,
-                z,
-            )
-            return w, z
-
-        w, z = project_unbatched(yraw.x[0])
+        w = yraw.x[:, self.idxs_w]
+        z = yraw.x[:, self.idxs_z]
+        zero = jnp.zeros_like(w)
+        _max = lax.max(lax.max(w, z), zero)
+        w = jnp.select(_max == 0, zero, w)
+        z = jnp.select(_max == 0, zero, z)
+        w = jnp.select(_max == z, zero, w)
+        z = jnp.select(_max == w, zero, z)
 
         return yraw.update(x=yraw.x.at[:, self.idxs_w].set(w).at[:, self.idxs_z].set(z))
 
     @property
     def dim(self) -> int:
         """Return the dimension of the constraint set."""
-        return self.A.shape[-1]
+        return self.idxs_w.stop - self.idxs_z.start
 
     @property
     def n_constraints(self) -> int:
-        """Return the number of constraints."""
-        return self.A.shape[1]
+        """Return the number of constraints.
 
-    def cv(self, inp: ProjectionInstance) -> jnp.ndarray:
+        If w has size n, then there are 3*n constraints:
+        - z_i >= 0      forall n
+        - w_i >= 0      forall n
+        - z_i w_i = 0   forall n
+        """
+        return 3 * (self.idxs_w.stop - self.idxs_z.start)
+
+    def cv(self, yraw: ProjectionInstance) -> jnp.ndarray:
         """Compute the constraint violation.
 
         Args:
@@ -116,6 +114,14 @@ class CrossConstraint(Constraint):
             jnp.ndarray: The constraint violation for each point in the batch.
                 Shape (batch_size, 1, 1).
         """
-        b, A, _ = self.get_params(inp)
+        w = yraw.x[:, self.idxs_w]
+        z = yraw.x[:, self.idxs_z]
 
-        return jnp.linalg.norm(A @ inp.x - b, ord=jnp.inf, axis=1, keepdims=True)
+        return jnp.concatenate(
+            [
+                lax.max(-w, 0),
+                lax.max(-z, 0),
+                jnp.abs(w * z),
+            ],
+            axis=1,
+        )
